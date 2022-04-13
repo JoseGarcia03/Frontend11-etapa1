0ce11542e2759632dccf637493c96ac57a88a8bd
JoseGarcia03 <jose.garcia@agileinnova.org>
2022-04-13 18:51:54 -0500

    ejercicio parte 2

---
+++ Sesión 4/ejercicio/scripts/index.js
@@ -0,0 +1,9 @@
+// Una universidad tiene el siguiente plan de tarifas de acuerdo a la
+// cantidad de créditos que el estudiante desee matricular: Si un
+// estudiante matrícula menos o igual de 6 créditos tiene el 50% de
+// descuento sobre el valor de la matrícula, si matrícula más de 6
+// pero menos o igual de 10, tiene un 25% de descuento sobre el
+// valor de la matrícula, y si el estudiante matricula más de 10
+// créditos deberá pagar el total del valor de la matrícula. Hacer un
+// programa que calcule el valor a pagar de un estudiante teniendo
+// en cuenta que el valor de la matrícula es de $900.000
--- Sesión 4/practica/scripts/index.js
+++ Sesión 4/practica/scripts/index.js
@@ -1,7 +1,10 @@
 
 let btn = document.getElementById("btn")
-const input = document.getElementById('name')
 const form = document.getElementById('form')
+
+// Capuramos los inputs del formulario
+const input = document.getElementById('name')
+const lastName = document.getElementById('lastName')
 
 // Escuchar evento con funcion flecha
 // btn.addEventListener('click', ()=>{
@@ -32,8 +35,11 @@ const form = document.getElementById('form')
 // })
 
 // Evento Blur
-// input.addEventListener('blur', ()=> {
-//     input.style.background = 'red'
+// input.addEventListener('blur', (e)=> {
+//     if (input.value.length < 5) {
+//         alert('La contraseña debe ser minimo de 6 letras')
+//         input.style.background = 'red'
+//     }
 // })
 
 // document.addEventListener('DOMContentLoaded', ()=> {
@@ -41,12 +47,75 @@ const form = document.getElementById('form')
 // })
 
 // Evento Change
-input.addEventListener('change', (x)=> {
-    console.log(x.target.value)
+input.addEventListener('change', ({ target })=> {
+    console.log(target.value)
 })
 
+
 // Evento submit
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-    console.log("Hola")
-})
+// form.addEventListener('submit', function(e){
+//     e.preventDefault()
+//     if (input.value, lastName.value === "") {
+//         alert('Debes llenar todos los campos')
+//     }
+//     console.log(typeof(input.value))
+// })
+
+// OBJETOS
+let heigth = "200cm"
+
+let geekGirl = {
+    name: "Maria",
+    age: 31,
+    "is married": false
+}
+
+let ventilador = {
+    altura: heigth,
+    color: "negro",
+    potencia: "100W",
+    helices: { 
+        tamaño: "10cm",
+        material: "plastico",
+        color: "blanco"
+    },
+    precio: {
+        precio1: 20,
+        precio2: 40
+    }
+}
+
+// console.log(ventilador.helices.hasOwnProperty("material"))
+
+// console.log(ventilador.color)
+
+// ventilador.color = "azul"
+
+// console.log(ventilador.color)
+
+// let mentor = {}
+
+// console.log(mentor)
+
+// mentor.nombre = "Jose"
+// mentor.edad = 22
+
+// console.log(mentor.hasOwnProperty("edad"))
+
+// delete mentor.edad
+
+// console.log(mentor.hasOwnProperty("edad"))
+
+
+// DESESTRUCTURACION
+
+let miObjeto = {
+    tamano: 'Grande',
+    color: "Azul",
+    precio: 2000,
+    cantidad: 7
+}
+
+const { tamano, color } = miObjeto
+
+console.log(tamano, color)
